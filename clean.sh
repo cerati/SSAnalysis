76d6286f00293fb975ba84f92a3fc1fb027c55bc
Giuseppe Cerati <giuseppe.cerati@cern.ch>
2015-02-06 16:29:32 +0100

    check if files exist

--- clean.sh
+++ clean.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-rm roostats-* *.*~ higgsCombineTest.Asymptotic.mH120.root
- 
+if [ -e *.*~ ] 
+then rm *.*~
+fi
+
+if [ -e roostats-* ] 
+then rm roostats-*
+fi
+
+if [ -e higgsCombineTest.Asymptotic.mH120.root ] 
+then higgsCombineTest.Asymptotic.mH120.root
+fi
